de3321e6cc4a9092738e0c3a1e725b1c24590b9d
sigmotoa <si.galvismotoa@gmail.com>
2024-08-21 16:30:16 -0500

    Reading the keyboard

--- Hello.java
+++ Hello.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 class Hello {
 
 public static void main(String[] args) {
 
+Scanner leer = new Scanner(System.in);
 System.out.println ("Hello desde Block notas");
+String name;
+System.out.println("Type your name:");
+name = leer.next();
+System.out.println(name);
 }
 }
